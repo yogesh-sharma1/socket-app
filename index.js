ea991309274ceb90aa08a3e8c483faa8219e6d8a
pw11-085 <yogi.krsh22@gmail.com>
2024-06-19 00:10:16 +0530

    Initialized the project : socket-chat-app

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { createServer } = require("node:http");
+const { join } = require("node:path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("A socket connect!");
+});
+
+server.listen(3000, () => {
+  console.log("Server Is running http://localhost:3000");
+});
